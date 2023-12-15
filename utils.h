98de4498c7a33960893835f064c60b151176acb8
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2023-12-15 14:03:35 -0300

    ; kkkkkkkk

--- utils.h
+++ utils.h
@@ -6,7 +6,7 @@
 int FileIsNull(FILE *input);
 int removerX(char *str);
 void removerNewLine(char *str);
-unsigned int binarioParaInteiro(const char *strBinario)
+unsigned int binarioParaInteiro(const char *strBinario);
 void line (char *symbols, int length);
 
 #endif
